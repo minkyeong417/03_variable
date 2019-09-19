e2435c4104e715a2c3bf5786825ba390d45eeca1
minkyeong417 <55428050+minkyeong417@users.noreply.github.com>
2019-09-19 15:31:52 +0900

    02

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello world\n");
+	int x;
+	short y;
+	float z;
+	double w;
+	
+	printf("bytes of int : %d\n",sizeof(x));
+	printf("bytes of short : %d\n",sizeof(y));
+	printf("bytes of float : %d\n",sizeof(z));
+	printf("bytes of double : %d\n",sizeof(w));
 	
 	return 0;
 }
